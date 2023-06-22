c52d67205af07502f9372278507c6fd8c24862fb
mbecker20 <becker.maxh@gmail.com>
2023-06-22 19:32:54 +0000

    add lib functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,35 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use anyhow::Context;
+use serde::Deserialize;
+use vaultrs::{
+    auth::approle,
+    client::{Client, VaultClient, VaultClientSettingsBuilder},
+    kv2,
+};
+
+#[derive(Deserialize)]
+struct Env {
+    vault_role_id: String,
+    vault_secret_id: String,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Deserialize, Debug, Clone)]
+pub struct Credentials {
+    pub username: String,
+    pub password: String,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub async fn load_credentials(mount: &str, path: &str) -> anyhow::Result<Credentials> {
+    let env: Env = envy::from_env().context("failed to load vault credentials from env. expected to find $VAULT_ROLE_ID and $VAULT_SECRET_ID")?;
+
+    let mut vault = VaultClient::new(VaultClientSettingsBuilder::default().build()?)?;
+
+    let auth_info = approle::login(&vault, "approle", &env.vault_role_id, &env.vault_secret_id)
+        .await
+        .context("failed to login via approle")?;
+
+    vault.set_token(&auth_info.client_token);
+
+    kv2::read(&vault, mount, path).await.context(format!(
+        "failed to load credentials | mount: {mount} | path: {path}"
+    ))
 }
